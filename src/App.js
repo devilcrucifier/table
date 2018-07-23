4af19869672ae50034d0fdbde7daa7bb2e19e78b
devilcrucifier <amitbvp13@gmail.com>
2018-07-23 16:43:31 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactTable from 'react-table'
+import "react-table/react-table.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      tableData: [],
+    };
+  }
+
+  componentDidMount() {    
+    var that = this;
+    var url = 'https://jsonplaceholder.typicode.com/posts'
+  
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      that.setState({ tableData: data });
+    });
+  }
+  render() {
+    const data = Object.values(this.state.tableData);
+    const columns = [
+      {
+        Header: '#',
+        accessor: 'id',
+        width: 100,
+      },
+      {
+        Header: 'Title',
+        accessor: 'title'
+      },
+      {
+        Header: 'User Id',
+        accessor: 'userId',
+        width: 150,
+        },
+      {
+        Header: 'Size',
+        id: 'body',
+        width: 150,
+        Cell: data => data.original.body.length
+      },
+    ];
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={columns}
+          className="-striped -highlight"
+        />
+    </div>
+    );
+  }
+}
+
+export default App;
